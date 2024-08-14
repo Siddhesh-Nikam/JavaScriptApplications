67dd3702565c2f182732c5256ea964af5f8e7341
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-08-14 14:32:54 +0530

    Add files via upload

---
+++ Arrays_Clone_6.js
@@ -0,0 +1,19 @@
+
+console.log("Arrays_Clone_6");
+
+let arr1 = [1,2,3,4,5];
+
+let arr2 = [6,7,8,9,10];
+
+// arr1.push(arr2);
+// console.log(arr1);
+
+let arr3 = [...arr1,...arr2];
+
+console.log(arr3);
+
+
+// In JavaScript, cloning an array means creating a new array with the same elements as the original array.
+
+
+
